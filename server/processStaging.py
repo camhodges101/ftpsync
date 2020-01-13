778c333cea7a50323dbae08e9a1d2f7f6f97233c
cameron <camhodges@gmail.com>
2020-01-13 00:49:22 +0000

    added folders

--- server/processStaging.py
+++ server/processStaging.py
@@ -4,10 +4,11 @@ import hashlib
 import os
 
 def confirmfolder(targetdirectory):
-    
+
     base=['storage']
     base.extend(targetdirectory)
     targetdirectory=base
+    targetdirectory=list(filter(None,targetdirectory))
     startingwd=os.getcwd()
     for folder in targetdirectory:
         if os.path.isdir(folder):
@@ -15,11 +16,25 @@ def confirmfolder(targetdirectory):
         else:
             os.mkdir(folder)
             os.chdir(folder)
-    
-    os.chdir(startingwd)
-   
 
-def correctserverpath(fullpath):
+    os.chdir(startingwd)
+
+def updateServerManifest(filepath, filehash,lastmodtime):
+    with open('/home/cameron/ftpsyncstorage/serverManifest.json','r') as infile:
+        serverManifest=json.load(infile)
+    serverManifest[filepath]={'hash':filehash,'modtime':lastmodtime,'flags':[0,0,0],'repeat':False}
+    with open('/home/cameron/ftpsyncstorage/serverManifest.json','w') as outfile:
+        json.dump(serverManifest,outfile)
+
+def updateTransferManifest(filehash):
+    with open('/home/cameron/ftpsyncstorage/.client01Control/transferManifest.json','r') as infile:
+        transferManifest=json.load(infile)
+    transferManifest['transfer'][filehash]['transferred']=True
+    with open('/home/cameron/ftpsyncstorage/.client01Control/transferManifest.json','w') as outfile:
+        json.dump(transferManifest,outfile)
+
+
+def  correctserverpath(fullpath):
 
     return 'storage'+fullpath
 
@@ -27,17 +42,19 @@ def correctserverpath(fullpath):
 
 def gethash(filename):
     sha256_hash = hashlib.sha256()
- 
+
     with open(filename,"rb") as f:
         # Read and update hash string value in blocks of 4K
         for byte_block in iter(lambda: f.read(4096),b""):
             sha256_hash.update(byte_block)
         return sha256_hash.hexdigest()
 
-with open('/home/pi/ftpsync/.client01Control/transferManifest.json') as infile:
+os.chdir('/home/cameron/ftpsyncstorage/')
+
+with open('/home/cameron/ftpsyncstorage/.client01Control/transferManifest.json') as infile:
 	transferManifest=json.load(infile)
 #print(transferManifest)
-	
+
 for idx, filehash in enumerate(transferManifest['transfer']):
     if transferManifest['transfer'][filehash]['transferred']==True and transferManifest['transfer'][filehash]['Processed']==False and gethash('.client01Staging/'+filehash)==filehash:
         paths=transferManifest['transfer'][filehash]['path']
@@ -45,5 +62,7 @@ for idx, filehash in enumerate(transferManifest['transfer']):
             print(path)
             confirmfolder(path.split('/')[:-1])
             su.copy('.client01Staging/'+filehash,correctserverpath(path))
+            updateServerManifest(path,filehash,transferManifest['transfer'][filehash]['lastmodtime'])
+            updateTransferManifest(filehash)
         os.remove('.client01Staging/'+filehash)
-	
+
