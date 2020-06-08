122c8755b09d6a65d8d2756197c9cd94c96c0e82
Cameron <camhodges@gmail.com>
2020-06-08 09:19:29 +0800

    electronGUI

---
+++ client/electronGUI/main.js
@@ -0,0 +1,63 @@
+
+const electron = require('electron');
+const path = require('path');
+
+
+const {app, BrowserWindow,ipcMain, ipcRenderer} = electron;
+
+let mainWindow;
+app.whenReady().then(() => {
+  createWindow()
+  console.log("creating main window")
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+
+  })
+})
+function createWindow () {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 400,
+    height: 800,
+    resizable: false,
+    frame: false,
+    backgroundColor: '#2e2c29',
+    webPreferences: {nodeIntegration: true,
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html')
+
+ 
+}
+
+const mainMenuTemplate = [
+  {
+      label:"File",
+      submenu:[
+
+
+          {
+              label:'Quit',
+              accelerator: process.platform == 'darwin' ? 'Command+Q' : 
+              'Ctrl+Q',
+              click(){
+                app.quit();  
+              }
+          }
+
+      ]
+  }
+];
+
+app.on('window-all-closed', function () {
+
+  if (process.platform !== 'darwin') app.quit()
+});
+
+
+
---
+++ client/electronGUI/stateHandler.js
@@ -0,0 +1,72 @@
+const dgram = require('dgram');
+const server = dgram.createSocket('udp4');
+
+const socket = require('socket.io')
+
+function saveStaticDataToFile() {
+  var blob = new Blob(["Welcome to Websparrow.org."],
+      { type: "text/plain;charset=utf-8" });
+  saveAs(blob, "static.txt");
+}
+
+
+function pauseSync(){
+  var PRButton = document.getElementById("PauseButton")
+  console.log("button pressed");
+  if (PRButton.innerHTML == "Pause Sync"){
+    PRButton.innerHTML = "Resume Sync";
+  } else if (PRButton.innerHTML == "Resume Sync"){
+    PRButton.innerHTML = "Pause Sync";
+  };
+};
+
+server.on('error', (err) => {
+  console.log(`server error:\n${err.stack}`);
+  server.close();
+});
+
+server.on('message', (msg, rinfo) => {
+  
+  const msgstring = msg.toString();
+  const msgID = msgstring.split(",")[0];
+  if (msgID == "0x10"){
+    const fileProgress = msgstring.split(",")[1];
+    const totalFiles = msgstring.split(",")[2];
+    const transferMode = msgstring.split(",")[3];
+    const serverHost = msgstring.split(",")[4];
+    const connectionState = msgstring.split(",")[5];
+    var FILEPROGRESS = document.getElementById("transferfileCount");
+    
+    var TRANSFERMODE = document.getElementById("transferState");
+    var SERVERHOST = document.getElementById("Hostname");
+    var CONNECTIONSTATE = document.getElementById("connnect-state");
+    if (transferMode != "Transferring") {
+      FILEPROGRESS.textContent = `Waiting to start transfer`;
+    } else {
+        FILEPROGRESS.textContent = `Progress: ${fileProgress} / ${totalFiles}`;
+      };
+    
+    TRANSFERMODE.textContent = `Transfer Mode:  ${transferMode}`;
+    SERVERHOST.textContent = `Server Host:  ${serverHost}`;
+    CONNECTIONSTATE.textContent = `Connection State:  ${connectionState}`;  
+  }
+  if (msgID == "0x20"){
+    const fileliststring = msgstring.split(",")[1];
+    const fileList = fileliststring.split(";").join("<br />");
+    var UnsynedFiles = document.getElementById("Unsyncedfiles");
+    UnsynedFiles.innerHTML = fileList;
+  }
+});
+
+server.on('listening', () => {
+  const address = server.address();
+  console.log(`server listening ${address.address}:${address.port}`);
+});
+
+server.bind(3001);
+var PRButton = document.getElementById("PauseButton")
+PRButton.addEventListener('click', () => {
+  
+  pauseSync();
+
+})
